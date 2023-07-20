67baa81b33618a3d58268e6cf62e982137117630
PatipatPhi <p.phiphithanachod@accenture.com>
2023-07-20 14:54:52 +0700

    [FIX] - init dagger and added view binding

--- app/src/main/java/eu/tutorials/testapp/AppComponent.kt
+++ app/src/main/java/eu/tutorials/testapp/AppComponent.kt
@@ -1,24 +1,15 @@
 package eu.tutorials.testapp
 
 import android.app.Application
+import android.content.Context
 import dagger.BindsInstance
 import dagger.Component
 import dagger.android.support.AndroidSupportInjectionModule
 import javax.inject.Singleton
 
 @Singleton
-@Component(
-    modules = [
-        AndroidSupportInjectionModule::class
-    ])
+@Component(modules = [AppModule::class])
 interface AppComponent {
-    @Component.Builder
-    interface Builder {
-        @BindsInstance
-        fun application(application: Application): Builder
-
-        fun build(): AppComponent
-    }
-
-    fun inject(awesomeApplication: AwesomeApplication)
+    fun context(): Context
+    fun inject(context: Context)
 }
